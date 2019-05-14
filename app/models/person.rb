429c49048a56d18f282097d4ae05cc0f1470e727
Nicki Nixon <nickitza@gmail.com>
2019-05-14 16:02:19 -0600

    formatting view pages, add quote model

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,12 @@
 class Person < ApplicationRecord
+  has_many :quotes, dependent: :destroy
+
+
+  validates :first_name, presence: true
+
+  def full_name
+    "#{self.last_name}, #{self.first_name}"
+  end
+
+
 end
